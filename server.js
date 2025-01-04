10d884813f57eee13515c3b3b83828401e0a0683
lionel delbe <progsystem@gmail.com>
2025-01-04 00:07:09 -0500

    Initial commit from your app

--- server.js
+++ server.js
@@ -5,11 +5,20 @@ const path = require('path');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  let filePath = '.' + req.url;
-  if (filePath === './') {
-    filePath = './static/index.html';
+  // Serve static files
+  if (req.url === '/') {
+    serveFile(res, './static/index.html', 'text/html');
+    return;
   }
 
+  // Serve JSON data directly
+  if (req.url === '/data/countries_cities.json') {
+    serveFile(res, './data/countries_cities.json', 'application/json');
+    return;
+  }
+
+  // Serve other static files
+  const filePath = '.' + req.url;
   const extname = String(path.extname(filePath)).toLowerCase();
   const mimeTypes = {
     '.html': 'text/html',
@@ -17,9 +26,10 @@ const server = http.createServer((req, res) => {
     '.css': 'text/css',
     '.json': 'application/json',
   };
+  serveFile(res, filePath, mimeTypes[extname] || 'application/octet-stream');
+});
 
-  const contentType = mimeTypes[extname] || 'application/octet-stream';
-
+function serveFile(res, filePath, contentType) {
   fs.readFile(filePath, (err, content) => {
     if (err) {
       if (err.code === 'ENOENT') {
@@ -34,7 +44,7 @@ const server = http.createServer((req, res) => {
       res.end(content, 'utf-8');
     }
   });
-});
+}
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
